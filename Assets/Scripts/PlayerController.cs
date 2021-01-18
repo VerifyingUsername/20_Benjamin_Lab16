cfb080344c0216dd92e0b137efd00cd19a0c90dd
VerifyingUsername <72852459+VerifyingUsername@users.noreply.github.com>
2021-01-18 11:58:25 +0800

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+
+    public GameObject bulletPrefab;
+    public GameObject bulletSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * -speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime *- rotateSpeed, 0));
+        }
+
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, Time.deltaTime * rotateSpeed, 0));
+        }
+
+        if( Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, bulletSpawn.transform.position, transform.rotation);
+        }
+    }
+
+    
+}
